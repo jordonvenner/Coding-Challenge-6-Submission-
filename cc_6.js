bb0dcd5d2f05f7cfd3b52a02a55e6b40b4c6f36c
Jordon Venner <jordonvenner@usf.edu>
2025-02-09 16:04:13 -0500

    Task 2 - Sales Tax Computation.

--- cc_6.js
+++ cc_6.js
@@ -8,3 +8,15 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
 console.log(calculateProfit(20, 30, 100)); // Output: "Total Profit: $1000"
 
 console.log(calculateProfit(50, 70, 200)); // Output: "Total Profit: $4000"
+
+
+//Task 2: Function Expression
+
+const calculateSalesTax = function(amount, taxRate) {
+    const salesTax = amount * taxRate;
+    return `Sales Tax: $${salesTax}`;
+};
+
+console.log(calculateSalesTax(100, 0.07));
+
+console.log(calculateSalesTax(500, 0.1));
