f894e7e30d5b9486999555943ed6606dd2704a41
Jordon Venner <jordonvenner@usf.edu>
2025-02-09 16:17:53 -0500

    Task 4 - Subscription Pricing Model.

--- cc_6.js
+++ cc_6.js
@@ -47,3 +47,30 @@ console.log(`Bonus: $${bonus1}`);
 
 const bonus2 = calculateBonus(7000, "Good"); // Expected: 700
 console.log(`Bonus: $${bonus2}`);
+
+
+//Task 4: Parameters and Arguments
+
+function calculateSubscriptionCost(plan, months, discount = 0) {
+    let monthlyPrice = 0;
+
+    if (plan === "Basic") {
+        monthlyPrice = 10;
+    } else if (plan === "Premium") {
+        monthlyPrice = 20;
+    } else if (plan === "Enterprise") {
+        monthlyPrice = 50;
+    } else {
+        console.log("Invalid plan selected.");
+        return;
+    }
+
+    let totalCost = (monthlyPrice * months) - discount;
+    return totalCost;
+}
+
+const cost1 = calculateSubscriptionCost("Basic", 6, 10); // Expected: 50
+console.log(`Total Cost: $${cost1}`);
+
+const cost2 = calculateSubscriptionCost("Premium", 12, 0); // Expected: 240
+console.log(`Total Cost: $${cost2}`);
