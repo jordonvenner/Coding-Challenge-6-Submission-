11221292ce69b0ebcecc2ccbd89ae558723d03a6
Jordon Venner <jordonvenner@usf.edu>
2025-02-09 16:08:18 -0500

    Task 3 - Employee Bonus Calculation.

--- cc_6.js
+++ cc_6.js
@@ -20,3 +20,30 @@ const calculateSalesTax = function(amount, taxRate) {
 console.log(calculateSalesTax(100, 0.07));
 
 console.log(calculateSalesTax(500, 0.1));
+
+
+//Task 3: Arrow Function
+
+const functionName = (parameters) => {
+    return something;
+};
+
+const calculateBonus = (salary, performanceRating) => {
+    let bonusPercentage = 0;
+
+    if (performanceRating === "Excellent") {
+        bonusPercentage = 0.2;
+    } else if (performanceRating === "Good") {
+        bonusPercentage = 0.1;
+    } else if (performanceRating === "Average") {
+        bonusPercentage = 0.05;
+    }
+
+    return salary * bonusPercentage;
+};
+
+const bonus1 = calculateBonus(5000, "Excellent"); // Expected: 1000
+console.log(`Bonus: $${bonus1}`);
+
+const bonus2 = calculateBonus(7000, "Good"); // Expected: 700
+console.log(`Bonus: $${bonus2}`);
